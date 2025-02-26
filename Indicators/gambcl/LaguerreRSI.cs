6bd139bd07a29653c6433fed2eeb7b340cd01cef
Charles Gamble <gambcl@proton.me>
2025-02-26 21:59:26 +0000

    Fixed property ordering in LaguerreRSI indicator

--- Indicators/gambcl/LaguerreRSI.cs
+++ Indicators/gambcl/LaguerreRSI.cs
@@ -1,24 +1,11 @@
 #region Using declarations
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Xml.Serialization;
-using NinjaTrader.Cbi;
 using NinjaTrader.Gui;
 using NinjaTrader.Gui.Chart;
-using NinjaTrader.Gui.SuperDom;
-using NinjaTrader.Gui.Tools;
-using NinjaTrader.Data;
-using NinjaTrader.NinjaScript;
-using NinjaTrader.Core.FloatingPoint;
-using NinjaTrader.NinjaScript.DrawingTools;
 #endregion
 
 //This namespace holds Indicators in this folder and is required. Do not change it. 
@@ -383,12 +370,12 @@ namespace NinjaTrader.NinjaScript.Indicators.gambcl
         { get; set; }
 
         [NinjaScriptProperty]
-        [Display(Name = "Enter Oversold", Description = "Alert sound used when Laguerre RSI enters the oversold region.", Order = 2, GroupName = "Alerts")]
+        [Display(Name = "Enter Oversold", Description = "Alert sound used when Laguerre RSI enters the oversold region.", Order = 4, GroupName = "Alerts")]
         public string EnterOversoldAlert
         { get; set; }
 
         [NinjaScriptProperty]
-        [Display(Name = "Exit Oversold", Description = "Alert sound used when Laguerre RSI exits the oversold region.", Order = 3, GroupName = "Alerts")]
+        [Display(Name = "Exit Oversold", Description = "Alert sound used when Laguerre RSI exits the oversold region.", Order = 5, GroupName = "Alerts")]
         public string ExitOversoldAlert
         { get; set; }
 
